2d3a299a8502b6665e2a53fe4dd149eed93b9344
aaronbss <55691291+aaronbss@users.noreply.github.com>
2019-10-05 17:30:13 +0100

    HCSR04_CODE.ino

---
+++ HCSR04_CODE/HCSR04_CODE.ino
@@ -0,0 +1,42 @@
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+const int buzz = 4;
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+pinMode(buzz, OUTPUT);
+
+}
+
+void loop() {
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+
+if(distance<=5)
+{
+    digitalWrite(buzz,HIGH);
+}
+
+
+}
+// Prints the distance on the Serial Monitor
+
+
